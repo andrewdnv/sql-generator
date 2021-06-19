78a1285bebb18bbccec59b287e25a1e21ec495f4
andrew <october072017@yandex.ru>
2021-06-19 14:15:34 +0600

    FB-001: Table class structure.

--- src/main/java/io/xpnnt/devtools/sql/generator/table/spi/Table.kt
+++ src/main/java/io/xpnnt/devtools/sql/generator/table/spi/Table.kt
@@ -1,3 +1,13 @@
 package io.xpnnt.devtools.sql.generator.table.spi
 
-interface Table
+import io.xpnnt.devtools.sql.generator.table.Column
+
+interface Table {
+
+    val name: String
+
+    val alias: String
+
+    fun allColumns(): List<Column>
+
+}
