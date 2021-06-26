f3e54f4d63fbf205e9d57ff24c79cf8f4ce039e0
andrew <october072017@yandex.ru>
2021-06-26 13:07:13 +0600

    Added GroupedConditionPrintTask.

---
+++ src/main/java/io/xpnnt/devtools/sql/generator/task/print/factory/ConditionPrintTaskFactory.kt
@@ -0,0 +1,21 @@
+package io.xpnnt.devtools.sql.generator.task.print.factory
+
+import io.xpnnt.devtools.sql.generator.condition.Condition
+import io.xpnnt.devtools.sql.generator.condition.GroupedCondition
+import io.xpnnt.devtools.sql.generator.condition.SimpleCondition
+import io.xpnnt.devtools.sql.generator.context.option.OptionName
+import io.xpnnt.devtools.sql.generator.task.print.GroupedConditionPrintTask
+import io.xpnnt.devtools.sql.generator.task.print.PrintTask
+import io.xpnnt.devtools.sql.generator.task.print.SimpleConditionPrintTask
+
+object ConditionPrintTaskFactory {
+
+    fun getTask(optionMap: Map<OptionName, Int>, condition: Condition): PrintTask {
+        return if (condition is SimpleCondition) {
+            SimpleConditionPrintTask(optionMap, condition)
+        } else {
+            GroupedConditionPrintTask(optionMap, condition as GroupedCondition)
+        }
+    }
+
+}
