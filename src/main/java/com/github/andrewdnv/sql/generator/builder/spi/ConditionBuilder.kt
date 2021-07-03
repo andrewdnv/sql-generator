93ba20ab7942a5e971b5f76dd65a50b9cbda7940
andrew <october072017@yandex.ru>
2021-07-03 13:04:29 +0600

    Added oneOf() and allOf() methods.

--- src/main/java/com/github/andrewdnv/sql/generator/builder/spi/ConditionBuilder.kt
+++ src/main/java/com/github/andrewdnv/sql/generator/builder/spi/ConditionBuilder.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.github.andrewdnv.sql.generator.builder.spi
 
 import com.github.andrewdnv.sql.generator.builder.api.CanPrecedeGroup
@@ -5,6 +7,8 @@ import com.github.andrewdnv.sql.generator.builder.api.CanPrecedeOrder
 import com.github.andrewdnv.sql.generator.builder.api.ClauseBuilder
 import com.github.andrewdnv.sql.generator.condition.Condition
 import com.github.andrewdnv.sql.generator.condition.ConditionType
+import com.github.andrewdnv.sql.generator.context.option.ConnectorOption
+import com.github.andrewdnv.sql.generator.context.option.OptionName
 import com.github.andrewdnv.sql.generator.table.spi.TableFactory
 import com.github.andrewdnv.sql.generator.task.print.ResultConditionPrintTask
 
@@ -12,6 +16,16 @@ interface ConditionBuilder<TF : TableFactory<TF, CB>, CB : ConditionBuilder<TF, 
     val type: ConditionType
 
     val conditions: MutableList<Condition>
+
+    fun oneOf(): CB {
+        ctx.optionMap.put(OptionName.RESULT_COMPARISON_CONNECTOR, ConnectorOption.OR.value)
+        return this as CB
+    }
+
+    fun allOf(): CB {
+        ctx.optionMap.put(OptionName.RESULT_COMPARISON_CONNECTOR, ConnectorOption.AND.value)
+        return this as CB
+    }
 
     override fun build() {
         val conditionClause = ResultConditionPrintTask(ctx.optionMap, type, conditions).print()
